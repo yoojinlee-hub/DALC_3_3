7a73e5ab516facab63ce320e6682347f8bea044a
Yoojin Lee <80568037+yoojinlee-hub@users.noreply.github.com>
2022-07-29 21:25:06 +0900

    update click-events

---
+++ js/change_stars_books.js
@@ -0,0 +1,540 @@
+//별 click event //ERROR
+var stars_checked=0;
+function checkStar1(num) {
+  var color = ['#F3B743','#000000'];
+  var s1 = document.getElementById('stars1_1').style;
+  var s2 = document.getElementById('stars1_2').style;
+  var s3 = document.getElementById('stars1_3').style;
+  var s4 = document.getElementById('stars1_4').style;
+  var s5 = document.getElementById('stars1_5').style;
+  if(num==1){
+    if(stars_checked==1){
+        s1.color=color[1];
+        s2.color=color[1];
+        s3.color=color[1];
+        s4.color=color[1];
+        s5.color=color[1];
+        stars_checked=0;
+    } 
+    if(s1.color=color[1]){
+        s1.color=color[0];
+        stars_checked = 1;
+    } 
+  }
+  if(num==2){
+    if(stars_checked==1){
+        s1.color=color[1];
+        s2.color=color[1];
+        s3.color=color[1];
+        s4.color=color[1];
+        s5.color=color[1];
+        stars_checked=0;
+    } 
+    if(s2.color=color[1]){
+        s1.color=color[0];
+        s2.color=color[0];
+        stars_checked = 1;
+    } 
+  }
+  if(num==3){
+    if(stars_checked==1){
+        s1.color=color[1];
+        s2.color=color[1];
+        s3.color=color[1];
+        s4.color=color[1];
+        s5.color=color[1];
+        stars_checked=0;
+    } 
+    if(s3.color=color[1]){
+        s1.color=color[0];
+        s2.color=color[0];
+        s3.color=color[0];
+        stars_checked = 1;
+    } 
+  }
+  if(num==4){
+    if(stars_checked==1){
+        s1.color=color[1];
+        s2.color=color[1];
+        s3.color=color[1];
+        s4.color=color[1];
+        s5.color=color[1];
+        stars_checked=0;
+    } 
+    if(s4.color=color[1]){
+        s1.color=color[0];
+        s2.color=color[0];
+        s3.color=color[0];
+        s4.color=color[0];
+        stars_checked = 1;
+    } 
+  }
+  if(num==5){
+    if(stars_checked==1){
+        s1.color=color[1];
+        s2.color=color[1];
+        s3.color=color[1];
+        s4.color=color[1];
+        s5.color=color[1];
+        stars_checked=0;
+    } 
+    if(s5.color=color[1]){
+        s1.color=color[0];
+        s2.color=color[0];
+        s3.color=color[0];
+        s4.color=color[0];
+        s5.color=color[0];
+        stars_checked = 1;
+    }
+  }
+}
+//2
+var stars_checked2=0;
+function checkStar2(num) {
+  var color = ['#F3B743','#000000'];
+  var s1 = document.getElementById('stars2_1').style;
+  var s2 = document.getElementById('stars2_2').style;
+  var s3 = document.getElementById('stars2_3').style;
+  var s4 = document.getElementById('stars2_4').style;
+  var s5 = document.getElementById('stars2_5').style;
+  if(num==1){
+    if(stars_checked2==1){
+        s1.color=color[1];
+        s2.color=color[1];
+        s3.color=color[1];
+        s4.color=color[1];
+        s5.color=color[1];
+        stars_checked2=0;
+    } 
+    if(s1.color=color[1]){
+        s1.color=color[0];
+        stars_checked2 = 1;
+    } 
+  }
+  if(num==2){
+    if(stars_checked2==1){
+        s1.color=color[1];
+        s2.color=color[1];
+        s3.color=color[1];
+        s4.color=color[1];
+        s5.color=color[1];
+        stars_checked2=0;
+    } 
+    if(s2.color=color[1]){
+        s1.color=color[0];
+        s2.color=color[0];
+        stars_checked2 = 1;
+    } 
+  }
+  if(num==3){
+    if(stars_checked2==1){
+        s1.color=color[1];
+        s2.color=color[1];
+        s3.color=color[1];
+        s4.color=color[1];
+        s5.color=color[1];
+        stars_checked2=0;
+    } 
+    if(s3.color=color[1]){
+        s1.color=color[0];
+        s2.color=color[0];
+        s3.color=color[0];
+        stars_checked2 = 1;
+    } 
+  }
+  if(num==4){
+    if(stars_checked2==1){
+        s1.color=color[1];
+        s2.color=color[1];
+        s3.color=color[1];
+        s4.color=color[1];
+        s5.color=color[1];
+        stars_checked2=0;
+    } 
+    if(s4.color=color[1]){
+        s1.color=color[0];
+        s2.color=color[0];
+        s3.color=color[0];
+        s4.color=color[0];
+        stars_checked2 = 1;
+    } 
+  }
+  if(num==5){
+    if(stars_checked2==1){
+        s1.color=color[1];
+        s2.color=color[1];
+        s3.color=color[1];
+        s4.color=color[1];
+        s5.color=color[1];
+        stars_checked2=0;
+    } 
+    if(s5.color=color[1]){
+        s1.color=color[0];
+        s2.color=color[0];
+        s3.color=color[0];
+        s4.color=color[0];
+        s5.color=color[0];
+        stars_checked2 = 1;
+    }
+  }
+}
+//3
+var stars_checked3=0;
+function checkStar3(num) {
+  var color = ['#F3B743','#000000'];
+  var s1 = document.getElementById('stars3_1').style;
+  var s2 = document.getElementById('stars3_2').style;
+  var s3 = document.getElementById('stars3_3').style;
+  var s4 = document.getElementById('stars3_4').style;
+  var s5 = document.getElementById('stars3_5').style;
+  if(num==1){
+    if(stars_checked3==1){
+        s1.color=color[1];
+        s2.color=color[1];
+        s3.color=color[1];
+        s4.color=color[1];
+        s5.color=color[1];
+        stars_checked3=0;
+    } 
+    if(s1.color=color[1]){
+        s1.color=color[0];
+        stars_checked3 = 1;
+    } 
+  }
+  if(num==2){
+    if(stars_checked3==1){
+        s1.color=color[1];
+        s2.color=color[1];
+        s3.color=color[1];
+        s4.color=color[1];
+        s5.color=color[1];
+        stars_checked3=0;
+    } 
+    if(s2.color=color[1]){
+        s1.color=color[0];
+        s2.color=color[0];
+        stars_checked3 = 1;
+    } 
+  }
+  if(num==3){
+    if(stars_checked3==1){
+        s1.color=color[1];
+        s2.color=color[1];
+        s3.color=color[1];
+        s4.color=color[1];
+        s5.color=color[1];
+        stars_checked3=0;
+    } 
+    if(s3.color=color[1]){
+        s1.color=color[0];
+        s2.color=color[0];
+        s3.color=color[0];
+        stars_checked3 = 1;
+    } 
+  }
+  if(num==4){
+    if(stars_checked3==1){
+        s1.color=color[1];
+        s2.color=color[1];
+        s3.color=color[1];
+        s4.color=color[1];
+        s5.color=color[1];
+        stars_checked3=0;
+    } 
+    if(s4.color=color[1]){
+        s1.color=color[0];
+        s2.color=color[0];
+        s3.color=color[0];
+        s4.color=color[0];
+        stars_checked3 = 1;
+    } 
+  }
+  if(num==5){
+    if(stars_checked3==1){
+        s1.color=color[1];
+        s2.color=color[1];
+        s3.color=color[1];
+        s4.color=color[1];
+        s5.color=color[1];
+        stars_checked3=0;
+    } 
+    if(s5.color=color[1]){
+        s1.color=color[0];
+        s2.color=color[0];
+        s3.color=color[0];
+        s4.color=color[0];
+        s5.color=color[0];
+        stars_checked3 = 1;
+    }
+  }
+}
+//4
+var stars_checked4=0;
+function checkStar4(num) {
+  var color = ['#F3B743','#000000'];
+  var s1 = document.getElementById('stars4_1').style;
+  var s2 = document.getElementById('stars4_2').style;
+  var s3 = document.getElementById('stars4_3').style;
+  var s4 = document.getElementById('stars4_4').style;
+  var s5 = document.getElementById('stars4_5').style;
+  if(num==1){
+    if(stars_checked3==1){
+        s1.color=color[1];
+        s2.color=color[1];
+        s3.color=color[1];
+        s4.color=color[1];
+        s5.color=color[1];
+        stars_checked4=0;
+    } 
+    if(s1.color=color[1]){
+        s1.color=color[0];
+        stars_checked4 = 1;
+    } 
+  }
+  if(num==2){
+    if(stars_checked4==1){
+        s1.color=color[1];
+        s2.color=color[1];
+        s3.color=color[1];
+        s4.color=color[1];
+        s5.color=color[1];
+        stars_checked4=0;
+    } 
+    if(s2.color=color[1]){
+        s1.color=color[0];
+        s2.color=color[0];
+        stars_checked4 = 1;
+    } 
+  }
+  if(num==3){
+    if(stars_checked4==1){
+        s1.color=color[1];
+        s2.color=color[1];
+        s3.color=color[1];
+        s4.color=color[1];
+        s5.color=color[1];
+        stars_checked4=0;
+    } 
+    if(s3.color=color[1]){
+        s1.color=color[0];
+        s2.color=color[0];
+        s3.color=color[0];
+        stars_checked4 = 1;
+    } 
+  }
+  if(num==4){
+    if(stars_checked4==1){
+        s1.color=color[1];
+        s2.color=color[1];
+        s3.color=color[1];
+        s4.color=color[1];
+        s5.color=color[1];
+        stars_checked4=0;
+    } 
+    if(s4.color=color[1]){
+        s1.color=color[0];
+        s2.color=color[0];
+        s3.color=color[0];
+        s4.color=color[0];
+        stars_checked4 = 1;
+    } 
+  }
+  if(num==5){
+    if(stars_checked4==1){
+        s1.color=color[1];
+        s2.color=color[1];
+        s3.color=color[1];
+        s4.color=color[1];
+        s5.color=color[1];
+        stars_checked4=0;
+    } 
+    if(s5.color=color[1]){
+        s1.color=color[0];
+        s2.color=color[0];
+        s3.color=color[0];
+        s4.color=color[0];
+        s5.color=color[0];
+        stars_checked4 = 1;
+    }
+  }
+}
+//5
+var stars_checked5=0;
+function checkStar5(num) {
+  var color = ['#F3B743','#000000'];
+  var s1 = document.getElementById('stars5_1').style;
+  var s2 = document.getElementById('stars5_2').style;
+  var s3 = document.getElementById('stars5_3').style;
+  var s4 = document.getElementById('stars5_4').style;
+  var s5 = document.getElementById('stars5_5').style;
+  if(num==1){
+    if(stars_checked5==1){
+        s1.color=color[1];
+        s2.color=color[1];
+        s3.color=color[1];
+        s4.color=color[1];
+        s5.color=color[1];
+        stars_checked5=0;
+    } 
+    if(s1.color=color[1]){
+        s1.color=color[0];
+        stars_checked5 = 1;
+    } 
+  }
+  if(num==2){
+    if(stars_checked5==1){
+        s1.color=color[1];
+        s2.color=color[1];
+        s3.color=color[1];
+        s4.color=color[1];
+        s5.color=color[1];
+        stars_checked5=0;
+    } 
+    if(s2.color=color[1]){
+        s1.color=color[0];
+        s2.color=color[0];
+        stars_checked5 = 1;
+    } 
+  }
+  if(num==3){
+    if(stars_checked5==1){
+        s1.color=color[1];
+        s2.color=color[1];
+        s3.color=color[1];
+        s4.color=color[1];
+        s5.color=color[1];
+        stars_checked5=0;
+    } 
+    if(s3.color=color[1]){
+        s1.color=color[0];
+        s2.color=color[0];
+        s3.color=color[0];
+        stars_checked5 = 1;
+    } 
+  }
+  if(num==4){
+    if(stars_checked5==1){
+        s1.color=color[1];
+        s2.color=color[1];
+        s3.color=color[1];
+        s4.color=color[1];
+        s5.color=color[1];
+        stars_checked5=0;
+    } 
+    if(s4.color=color[1]){
+        s1.color=color[0];
+        s2.color=color[0];
+        s3.color=color[0];
+        s4.color=color[0];
+        stars_checked5 = 1;
+    } 
+  }
+  if(num==5){
+    if(stars_checked5==1){
+        s1.color=color[1];
+        s2.color=color[1];
+        s3.color=color[1];
+        s4.color=color[1];
+        s5.color=color[1];
+        stars_checked5=0;
+    } 
+    if(s5.color=color[1]){
+        s1.color=color[0];
+        s2.color=color[0];
+        s3.color=color[0];
+        s4.color=color[0];
+        s5.color=color[0];
+        stars_checked5 = 1;
+    }
+  }
+}
+//6
+var stars_checked6=0;
+function checkStar6(num) {
+  var color = ['#F3B743','#000000'];
+  var s1 = document.getElementById('stars6_1').style;
+  var s2 = document.getElementById('stars6_2').style;
+  var s3 = document.getElementById('stars6_3').style;
+  var s4 = document.getElementById('stars6_4').style;
+  var s5 = document.getElementById('stars6_5').style;
+  if(num==1){
+    if(stars_checked6==1){
+        s1.color=color[1];
+        s2.color=color[1];
+        s3.color=color[1];
+        s4.color=color[1];
+        s5.color=color[1];
+        stars_checked6=0;
+    } 
+    if(s1.color=color[1]){
+        s1.color=color[0];
+        stars_checked6 = 1;
+    } 
+  }
+  if(num==2){
+    if(stars_checked6==1){
+        s1.color=color[1];
+        s2.color=color[1];
+        s3.color=color[1];
+        s4.color=color[1];
+        s5.color=color[1];
+        stars_checked6=0;
+    } 
+    if(s2.color=color[1]){
+        s1.color=color[0];
+        s2.color=color[0];
+        stars_checked6 = 1;
+    } 
+  }
+  if(num==3){
+    if(stars_checked6==1){
+        s1.color=color[1];
+        s2.color=color[1];
+        s3.color=color[1];
+        s4.color=color[1];
+        s5.color=color[1];
+        stars_checked6=0;
+    } 
+    if(s3.color=color[1]){
+        s1.color=color[0];
+        s2.color=color[0];
+        s3.color=color[0];
+        stars_checked6 = 1;
+    } 
+  }
+  if(num==4){
+    if(stars_checked6==1){
+        s1.color=color[1];
+        s2.color=color[1];
+        s3.color=color[1];
+        s4.color=color[1];
+        s5.color=color[1];
+        stars_checked6=0;
+    } 
+    if(s4.color=color[1]){
+        s1.color=color[0];
+        s2.color=color[0];
+        s3.color=color[0];
+        s4.color=color[0];
+        stars_checked6 = 1;
+    } 
+  }
+  if(num==5){
+    if(stars_checked6==1){
+        s1.color=color[1];
+        s2.color=color[1];
+        s3.color=color[1];
+        s4.color=color[1];
+        s5.color=color[1];
+        stars_checked6=0;
+    } 
+    if(s5.color=color[1]){
+        s1.color=color[0];
+        s2.color=color[0];
+        s3.color=color[0];
+        s4.color=color[0];
+        s5.color=color[0];
+        stars_checked6 = 1;
+    }
+  }
+}
--- js/checkmovie.js
+++ js/checkmovie.js
@@ -1,15 +1,118 @@
-//스와이퍼 click event //ERROR
-function checkClick(){
-    var color = ["#F3B743","#CFD8DC"]
-    var x = document.getElementsByClassName('check1')
-    
-    if(x.style.backgroundColor === color[0]){
-        x.style.backgroundColor = color[1];
-    }
-        
-    else if(x.style.backgroundColor === color[1])
-    {
-        x.style.backgroundColor = color[0];
-    }
-        
+//스와이퍼 click event
+var count = 0;
+function checkClick() {
+  var color = ['#CFD8DC','#F3B743'];
+  var bg = document.getElementById('click1').style;
+  if (count%2==0) {
+    bg.backgroundColor = color[1];
+    count+=1;
+  }
+  else { 
+    bg.backgroundColor = color[0];
+    count+=1;
+  }
+}
+var count2 = 0;
+function checkClick2() {
+  var color = ['#CFD8DC','#F3B743'];
+  var bg = document.getElementById('click2').style;
+  if (count2%2==0) {
+    bg.backgroundColor = color[1];
+    count2+=1;
+  }
+  else { 
+    bg.backgroundColor = color[0];
+    count2+=1;
+  }
+}
+var count3 = 0;
+function checkClick3() {
+  var color = ['#CFD8DC','#F3B743'];
+  var bg = document.getElementById('click3').style;
+  if (count3%2==0) {
+    bg.backgroundColor = color[1];
+    count3+=1;
+  }
+  else { 
+    bg.backgroundColor = color[0];
+    count3+=1;
+  }
+}
+var count4 = 0;
+function checkClick4() {
+  var color = ['#CFD8DC','#F3B743'];
+  var bg = document.getElementById('click4').style;
+  if (count4%2==0) {
+    bg.backgroundColor = color[1];
+    count4+=1;
+  }
+  else { 
+    bg.backgroundColor = color[0];
+    count4+=1;
+  }
+}
+var count5 = 0;
+function checkClick5() {
+  var color = ['#CFD8DC','#F3B743'];
+  var bg = document.getElementById('click5').style;
+  if (count5%2==0) {
+    bg.backgroundColor = color[1];
+    count5+=1;
+  }
+  else { 
+    bg.backgroundColor = color[0];
+    count5+=1;
+  }
+}
+var count6 = 0;
+function checkClick6() {
+  var color = ['#CFD8DC','#F3B743'];
+  var bg = document.getElementById('click6').style;
+  if (count6%2==0) {
+    bg.backgroundColor = color[1];
+    count6+=1;
+  }
+  else { 
+    bg.backgroundColor = color[0];
+    count6+=1;
+  }
+}
+var count7 = 0;
+function checkClick7() {
+  var color = ['#CFD8DC','#F3B743'];
+  var bg = document.getElementById('click7').style;
+  if (count7%2==0) {
+    bg.backgroundColor = color[1];
+    count7+=1;
+  }
+  else { 
+    bg.backgroundColor = color[0];
+    count7+=1;
+  }
+}
+var count8 = 0;
+function checkClick8() {
+  var color = ['#CFD8DC','#F3B743'];
+  var bg = document.getElementById('click8').style;
+  if (count8%2==0) {
+    bg.backgroundColor = color[1];
+    count8+=1;
+  }
+  else { 
+    bg.backgroundColor = color[0];
+    count8+=1;
+  }
+}
+var count9 = 0;
+function checkClick9() {
+  var color = ['#CFD8DC','#F3B743'];
+  var bg = document.getElementById('click9').style;
+  if (count9%2==0) {
+    bg.backgroundColor = color[1];
+    count9+=1;
+  }
+  else { 
+    bg.backgroundColor = color[0];
+    count9+=1;
+  }
 }
