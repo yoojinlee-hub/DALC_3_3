057567cac2ba8bb81ba755e3ddc05219ecce8194
Yoojin Lee <80568037+yoojinlee-hub@users.noreply.github.com>
2022-08-05 22:35:22 +0900

    Add files via upload

--- [AI part]/Distinguishing between foreign and domestic films.py
+++ [AI part]/Distinguishing between foreign and domestic films.py
@@ -11,18 +11,32 @@ from selenium.webdriver.common.keys import Keys
 import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from bs4 import BeautifulSoup
 
 # Webdriver 실행
-browser = webdriver.Chrome('D:/chromedriver.exe')
+driver = webdriver.Chrome('D:/chromedriver.exe')
 
 # Webdriver에서 네이버 페이지 접속
-browser.get('https://www.naver.com/')
+driver.get('https://www.naver.com/')
 time.sleep(0.3)
 
 countrys=[]
-search_box = browser.find_element(By.NAME, "query")
-search_box.send_keys(df["titles"][0]+" 정보")
-find_site = search_box.send_keys(Keys.RETURN)
-path = "#main_pack > div.sc_new.cs_common_module.case_empasis.color_14._au_movie_content_wrap > div.cm_content_wrap > div.cm_content_area._cm_content_area_info > div > div.detail_info > dl > div:nth-child(4) > dd"
-country = find_site.find_element_by_css_selector(path)
-print(country)
+for i in range(0,9990):
+    driver.get('https://www.naver.com/')
+    time.sleep(0.2)
+    search_box = driver.find_element(By.NAME, "query")
+    search_box.send_keys("영화 "+df["titles"][i] + " 정보")
+    search_box.send_keys(Keys.RETURN)
+    time.sleep(0.1)
+    path = "#main_pack > div.sc_new.cs_common_module.case_empasis.color_14._au_movie_content_wrap > div.cm_content_wrap > div.cm_content_area._cm_content_area_info > div > div.detail_info > dl"
+    html = driver.page_source
+    soup = BeautifulSoup(html, 'html.parser')
+    for j in soup.find_all("div",{"class":"info_group"}):
+        country = j.get_text()
+        print(country)
+        countrys.append(country)
+    if(i%1000==0):
+        print(i)
+
+
+
