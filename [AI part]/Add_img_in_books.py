78cf0269decd0ab43132d69aa8810fda6472c0ce
Yoojin Lee <80568037+yoojinlee-hub@users.noreply.github.com>
2022-08-09 20:54:58 +0900

    Add files via upload

--- [AI part]/Add_img_in_books.py
+++ [AI part]/Add_img_in_books.py
@@ -16,30 +16,36 @@ driver = webdriver.Chrome('D:/chromedriver.exe')
 
 #자동 검색 + 정보 저장
 imgs = []
+#자동 검색 + 정보 저장
+imgs = []
 for i in range(0,3770):
-    #초기화
+    # 초기화
     img_url = []
     A = []
     # Webdriver에서 네이버 페이지 접속
     driver.get('https://www.naver.com/')
     time.sleep(0.1)
     search_box = driver.find_element(By.NAME, "query")
-    if(type(df["title"][i])==str):
-        search_box.send_keys(df["title"][i] + " 책표지")
+    if (type(df["title"][i]) == str):
+        search_box.send_keys("책 "+df["title"][i])
         search_box.send_keys(Keys.RETURN)
         time.sleep(0.1)
         html = driver.page_source
         soup = BeautifulSoup(html, 'html.parser')
         soup1 = soup.find("img", {"class": "thumb api_get"})
-        A=str(soup1).split(" src=")
-        img_url=A[1].split("width=")[0]
-        img=img_url.replace('"','')
+        if soup1 != None:
+            A = str(soup1).split(" src=")
+            img_url = A[1].split("width=")[0]
+            img = img_url.replace('"', '')
+        else:
+            img = "ERROR"
         imgs.append(img)
     else:
-        img="ERROR"
+        img = "ERROR"
         imgs.append(img)
     print(i)
     print(img)
+
 
 #따로 인덱스에 저장
 df['imgs_url']=imgs
